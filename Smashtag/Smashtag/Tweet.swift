3d26f8827ea0d23a58302994435d4b7ea4021e6d
Ivan <ivan@air-ivan.dlink>
2016-09-10 16:06:37 +0300

    removingObject disabled (running through core data model)

--- Smashtag/Smashtag/Tweet.swift
+++ Smashtag/Smashtag/Tweet.swift
@@ -58,7 +58,7 @@ class Tweet: NSManagedObject {
                     print("decreasing mention \(mention.value!) rate to \(mentionRate)")
                     if mentionRate == 0 {
                         if let mentionToDelete = Mention.mentionWithMentionInfo(mention.value!, withMentionType: mention.type!, inManagedObjectContext: self.managedObjectContext!) {
-                            self.removeMentionsObject(mentionToDelete)
+//                            self.removeMentionsObject(mentionToDelete)
                             self.managedObjectContext!.deleteObject(mentionToDelete)
                             print("deleting mension from tweet: \(mentionToDelete.value!)")
                             return
